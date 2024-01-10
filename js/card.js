38a3c502fb4e76fa06526a2902c070365ce01ab8
chana-rozin <c0556787069@gmail.com>
2024-01-10 10:22:45 +0200

    add files and folders

---
+++ js/card.js
@@ -0,0 +1,12 @@
+class card{
+    constructor(color, filling, shape,count, img, id)
+    {
+     
+        this.color=color;
+        this.filling=filling;
+        this.shape=shape;
+        this.count=count;
+        this.img=img;
+        this.id=id;
+    }
+}
